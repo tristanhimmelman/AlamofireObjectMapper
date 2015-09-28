68ea24be2cf62c1f4bb341544529be02ea79f4a3
Andrea Tullis <andrea.tullis@sharewire.net>
2015-09-28 13:53:47 +0200

    Restored old methods and added test for array mapping with key path

--- AlamofireObjectMapper/AlamofireObjectMapper.swift
+++ AlamofireObjectMapper/AlamofireObjectMapper.swift
@@ -17,7 +17,7 @@ extension Request {
 	/**
 	Adds a handler to be called once the request has finished.
 	
-	- parameter completionHandler: A closure to be executed once the request has finished and the data has been mapped to a swift Object. The closure takes 2 arguments: the response object (of type Mappable) and any error produced while making the request
+	- parameter completionHandler: A closure to be executed once the request has finished and the data has been mapped to a swift Object with support for keypath mapping. The closure takes 2 arguments: the response object (of type Mappable) and any error produced while making the request
 	
 	- returns: The request.
 	*/
@@ -35,9 +35,34 @@ extension Request {
 	
 	- returns: The request.
 	*/
+    public func responseObject<T: Mappable>(completionHandler: (NSURLRequest, NSHTTPURLResponse?, T?, AnyObject?, ErrorType?) -> Void) -> Self {
+        return responseObject(nil, keyPath: nil, completionHandler: completionHandler)
+	}
+    
+    /**
+    Adds a handler to be called once the request has finished.
+    
+    - parameter completionHandler: A closure to be executed once the request has finished and the data has been mapped to a swift Object. The closure takes 2 arguments: the response object (of type Mappable) and any error produced while making the request
+    
+    - returns: The request.
+    */
+    
+    public func responseObject<T: Mappable>(completionHandler: (T?, ErrorType?) -> Void) -> Self {
+        return responseObject(nil,keyPath: nil) { (request, response, object, data, error) -> Void in
+            completionHandler(object, error)
+        }
+    }
+    
+    /**
+    Adds a handler to be called once the request has finished.
+    
+    - parameter completionHandler: A closure to be executed once the request has finished and the data has been mapped to a swift Object with support for keypath mapping. The closure takes 5 arguments: the URL request, the URL response, the response object (of type Mappable), the raw response data, and any error produced making the request.
+    
+    - returns: The request.
+    */
     public func responseObject<T: Mappable>(keyPath: String?,completionHandler: (NSURLRequest, NSHTTPURLResponse?, T?, AnyObject?, ErrorType?) -> Void) -> Self {
         return responseObject(nil, keyPath: keyPath, completionHandler: completionHandler)
-	}
+    }
 	
 	/**
 	Adds a handler to be called once the request has finished.
@@ -67,7 +92,7 @@ extension Request {
 	/**
 	Adds a handler to be called once the request has finished.
 	
-	- parameter completionHandler: A closure to be executed once the request has finished and the data has been mapped to a swift Object. The closure takes 2 arguments: the response array (of type Mappable) and any error produced while making the request
+	- parameter completionHandler: A closure to be executed once the request has finished and the data has been mapped to a swift Object with support for keypath mapping. The closure takes 2 arguments: the response array (of type Mappable) and any error produced while making the request
 	
 	- returns: The request.
 	*/
@@ -80,13 +105,38 @@ extension Request {
 	/**
 	Adds a handler to be called once the request has finished.
 	
-	- parameter completionHandler: A closure to be executed once the request has finished and the data has been mapped to a swift Object. The closure takes 5 arguments: the URL request, the URL response, the response array (of type Mappable), the raw response data, and any error produced making the request.
+	- parameter completionHandler: A closure to be executed once the request has finished and the data has been mapped to a swift Object with support for keypath mapping. The closure takes 5 arguments: the URL request, the URL response, the response array (of type Mappable), the raw response data, and any error produced making the request.
 	
 	- returns: The request.
 	*/
 	public func responseArray<T: Mappable>(keyPath: String?,completionHandler: (NSURLRequest, NSHTTPURLResponse?, [T]?, AnyObject?, ErrorType?) -> Void) -> Self {
         return responseArray(nil,keyPath: keyPath, completionHandler: completionHandler)
 	}
+    
+    /**
+    Adds a handler to be called once the request has finished.
+    
+    - parameter completionHandler: A closure to be executed once the request has finished and the data has been mapped to a swift Object. The closure takes 2 arguments: the response array (of type Mappable) and any error produced while making the request
+    
+    - returns: The request.
+    */
+    public func responseArray<T: Mappable>(completionHandler: ([T]?, ErrorType?) -> Void) -> Self {
+        return responseArray(nil,keyPath: nil) { (request, response, object, data, error) -> Void in
+            completionHandler(object, error)
+        }
+    }
+    
+    /**
+    Adds a handler to be called once the request has finished.
+    
+    - parameter completionHandler: A closure to be executed once the request has finished and the data has been mapped to a swift Object. The closure takes 5 arguments: the URL request, the URL response, the response array (of type Mappable), the raw response data, and any error produced making the request.
+    
+    - returns: The request.
+    */
+    public func responseArray<T: Mappable>(completionHandler: (NSURLRequest, NSHTTPURLResponse?, [T]?, AnyObject?, ErrorType?) -> Void) -> Self {
+        return responseArray(nil,keyPath: nil, completionHandler: completionHandler)
+    }
+
 	
 	/**
 	Adds a handler to be called once the request has finished.
