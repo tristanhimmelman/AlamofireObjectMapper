ece1acfc829a2688ca6dd4022f472cd4cd826e5c
Adin Cebic <adin.cebic@klika.ba>
2019-12-22 08:14:18 +0100

    Remove unnecessary code from package.swift

--- Package.swift
+++ Package.swift
@@ -2,14 +2,4 @@
 
 import PackageDescription
 
-let package = Package(
-    name: "AlamofireObjectMapper",
-    products: [
-        .library(
-            name: "AlamofireObjectMapper",
-            targets: ["AlamofireObjectMapper"]),
-    ],
-    dependencies: [
-    .package(url: "https://github.com/Alamofire/Alamofire", from: "5.1")
-    ]
-)
+let package = Package(name: "AlamofireObjectMapper")
