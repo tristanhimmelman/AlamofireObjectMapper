fb7586e56c2cec71db49d93709ff3696b56bb9d3
Adin Cebic <adin.cebic@klika.ba>
2019-12-22 08:20:21 +0100

    Add target description to package.swift file

--- Package.swift
+++ Package.swift
@@ -2,4 +2,19 @@
 
 import PackageDescription
 
-let package = Package(name: "AlamofireObjectMapper")
+let package = Package(
+    name: "AlamofireObjectMapper",
+    products: [
+        .library(
+            name: "AlamofireObjectMapper",
+            targets: ["AlamofireObjectMapper"]),
+    ],
+    targets: [
+        .target(
+            name: "AlamofireObjectMapper",
+            dependencies: []),
+        .testTarget(
+            name: "AlamofireObjectMapperTests",
+            dependencies: ["AlamofireObjectMapper"]),
+    ]
+)
