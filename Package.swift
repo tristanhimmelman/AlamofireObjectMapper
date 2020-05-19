299d7ecf1df57ed4cf6e2e2098ecd1a6e3efa181
Suraj Pathak <freesuraj@gmail.com>
2020-05-19 18:46:45 +1000

    make dependency https

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/Alamofire/Alamofire", "5.0.0"..."5.0.0"),
-        .package(url: "git@github.com:freesuraj/ObjectMapper.git", .branch("spm"))
+        .package(url: "https://github.com/freesuraj/ObjectMapper", .branch("spm"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
