472c3e3833c26f4c1b06852fda960b173f18e96e
Suraj Pathak <freesuraj@gmail.com>
2020-06-17 20:41:49 +1000

    Use tagged version of ObjectMapper

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/Alamofire/Alamofire", "5.0.0"..."5.0.0"),
-        .package(url: "https://github.com/freesuraj/ObjectMapper", .branch("spm"))
+        .package(url: "https://github.com/freesuraj/ObjectMapper", from: "4.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
