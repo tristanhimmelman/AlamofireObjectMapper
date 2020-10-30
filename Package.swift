bfce13be48cfebab37571b4c5d1a14f2bcc9679c
George Nick Gorzynski <georgegorzynski@me.com>
2020-10-30 01:07:13 +0000

    Added SPM Support

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "AlamofireObjectMapper",
+    platforms: [
+        .iOS(.v10),
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "AlamofireObjectMapper",
+            targets: ["AlamofireObjectMapper"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMinor(from: "5.0.0")),
+        .package(url: "https://github.com/tristanhimmelman/ObjectMapper.git", .upToNextMajor(from: "4.1.0")),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "AlamofireObjectMapper",
+            dependencies: ["AlamofireObjectMapper"],
+            path: "./AlamofireObjectMapper"),
+    ]
+)
