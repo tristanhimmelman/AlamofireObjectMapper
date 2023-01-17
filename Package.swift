a356518936b45c079fd24ff7fd8c82905bc81470
E. Brady Trexler <brady.trexler@wnco.com>
2023-01-17 10:26:41 -0500

    git@github and all package dependencies branch: refactor/spm-ssh

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
 
-        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.6.2"),
-        .package(url: "https://github.com/tristanhimmelman/ObjectMapper.git", from: "4.2.0")
+        .package(url: "git@github.com:Alamofire/Alamofire.git", from: "5.6.2"),
+        .package(url: "git@github.com:tristanhimmelman/ObjectMapper.git", from: "4.2.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
